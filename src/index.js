4049aca9b2f0bf6f04cd701cc521517cc3b0b864
sreedathan <sreedathang@gmail.com>
2020-11-26 00:25:57 +0530

    template change

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const createWindow = () => {
   });
 
   // and load the loginPage.html of the app.
-  mainWindow.loadFile(path.join(__dirname, './admin&doc/admin.html'));
+  mainWindow.loadFile(path.join(__dirname, './login/loginPage.html'));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
